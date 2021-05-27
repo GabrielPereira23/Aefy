55c060ceea59a5475b887e4be5a77f2ddef04dfa
GabrielAprendizado <gabrielgoulartpereira23@gmail.com>
2021-05-27 12:34:48 -0300

    Erros e validação
    Criei um método para apresentar erros no console, um para resetar os dados e iniciei a criação do método que ira fazer a validação dos dados.

--- Aefy.js
+++ Aefy.js
@@ -1,1 +1,35 @@
-// Lets go!
+class Aefy {
+  url = null;
+  method = null;
+  content = null;
+  contentString = null;
+  ready = false;
+  
+  error(code, message) {
+    this.reset();
+    console.error(`AEFY - Error ${code}, ${message}`);
+  }
+
+  reset() {
+    this.url = null;
+    this.method = null;
+    this.content = null;
+    this.contentString = null;
+    this.ready = false;
+  }
+
+  validation() {
+    if (!this.url) {
+      this.error(1, "Request url not defined");
+      return false;
+    }
+    if (!this.method) {
+      this.error(2, "Request method not defined");
+      return false;
+    }
+    if (!this.contentString) {
+      this.error(3, "Request content not defined");
+      return false;
+    }
+  }
+}
